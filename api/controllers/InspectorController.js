02329c1fa71d62324fce3d3e74bad0ce14be925c
lammaleung <lammaleung@gmail.com>
2020-02-15 01:30:00 +0800

    Add inspector function

--- api/controllers/InspectorController.js
+++ api/controllers/InspectorController.js
@@ -10,12 +10,93 @@ module.exports = {
         var inspectors = await Inspector.find();
         return res.json(inspectors);
     },
-    // create: function (req, res) {
+    signup: async function (req, res) {
+        if (req.method == "POST") {
+            _inspector = {};
+            sails.bcrypt = require('bcryptjs');
+            const saltRounds = 10;
+            const hash = await sails.bcrypt.hash(req.query.password, saltRounds);
+            _inspector.email = req.query.email;
+            _inspector.password = hash;
+            _inspector.name = req.query.name;
+            var model = await Inspector.create(_inspector).fetch();
+            if (!model) {
+                return res.send("Cannot create!");
+            }
+            return res.send("Successfully Created!");
+        }
+    },
+    login: function (req, res) {
+        if (req.method == "POST") {
+            Inspector.findOne({ email: req.query.email }).exec(function (err, inspector) {
+                // sails.log.debug(req.query.email);
+                // sails.log.debug("check: " + Inspector);
+                if (inspector == null)
+                    return res.send("No such user");
+                // Load the bcrypt module
+                var bcrypt = require('bcryptjs');
+                // // Generate a salt
+                var salt = bcrypt.genSaltSync(10);
+                if (!bcrypt.compareSync(req.query.password, inspector.password))
+                    return res.send("Wrong Password");
+                console.log("The session id " + req.session.id + " is going to be destroyed.");
+                req.session.regenerate(function (err) {
+                    console.log("The new session id is " + req.session.id + ".");
+                    //save to cookies
+                    req.session.uid = inspector.id;
+                    req.session.email = req.query.email;
+                    return res.send("login successfully.");
+                });
+            });
+        }
+    },
+    logout: function (req, res) {
+        console.log("The current session id " + req.session.id + " is going to be destroyed.");
+        req.session.destroy(function (err) {
+            return res.send("Log out successfully.");
+        });
+    },
+    // update function
+    update: async function (req, res) {
+        if (req.method == "POST") {
+            if (req.session.uid == null) {
+                return res.send("Log in first!");
+            }
+            var new_name = req.query.name;
+            var new_gender = req.query.gender;
+            var new_birth = req.query.birth;
+            var new_education = req.query.education;
+            var new_occupation = req.query.occupation;
+            await Inspector.update({ id: req.session.uid }).set({
+                "name": new_name, 
+                "gender": new_gender,
+                "birth": new_birth,
+                "education": new_education,
+                "occupation": new_occupation
+            })
+            return res.send("Successfully updated!");
+        }
+    },
+    // view current user's info
+    view: async function (req, res) {
+        if (!req.session.uid)
+            return res.send("Log in first!");
+        var model = await Inspector.findOne(req.session.uid);
 
-    //     if (req.method == "POST") {
-    //         Inspector.create(req.body.Inspector).exec(function (err, model) {
-    //             return res.send("Successfully Created!");
-    //         });
-    //     } 
-    // },
+        if (!model) return res.notFound();
+
+        return res.json(model);
+
+    },
+    getpaid: async function(req, res){
+        if (!req.session.uid)
+            return res.send("Log in first!");
+        var model = await Inspector.findOne(req.session.uid);
+        var topup_value = parseFloat(req.query.value);
+        var new_value = model.balance + topup_value;
+        await Inspector.update({ id: req.session.uid }).set({
+            "balance": new_value
+        })
+        return res.send("Get paid successfully!")
+    }
 };
